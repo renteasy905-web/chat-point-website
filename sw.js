2bf181aa3580d311bd9132ca5af9da3666eac566
renteasy905-web <renteasy905@gmail.com>
2026-01-04 08:03:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,3 @@
-// Minimal service worker to make PWA installable
 self.addEventListener('install', (e) => {
   self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-  // Do nothing – just required to register
 });
